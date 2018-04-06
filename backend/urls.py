c0c4d702a01024e3e60deb431eebaf9df5ae8fce
hxxyy <1483551722@qq.com>
2018-04-06 20:18:43 +0800

    complete index

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from backend import views
-
+from django.conf.urls import include #添加
 urlpatterns = [
     url(r'^$', views.index, name='index'), #链接示例API
-
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name="index.html")),
-    url(r'^api/', include('backend.urls', namespace='api')), #添加
+    url(r'^api/', include('backend.urls', namespace='api')),  # 添加
 ]
